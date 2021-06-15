d07e4c41108853c8402fedf1259cdaf2d850712c
Katleho-codes <67188889+Katleho-codes@users.noreply.github.com>
2021-06-15 08:59:00 +0200

    Add files via upload

---
+++ area_calculator.py
@@ -0,0 +1,7 @@
+# Create a file called “area_calculator.py” that when run  requests two value ( length & width ) 
+# and returns the area of a rectangle . 
+# Print a nice message with the answer.
+w = int(input('Type the width: '))
+l = int(input('Type the width: '))
+Area = w * l
+print('The area of the rectangle is ' + str(Area) + ' ' + 'square units')
